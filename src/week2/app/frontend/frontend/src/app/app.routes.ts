f75cec839a296f5f9b2b25f5e7ab66ced4efc4e5
SpiritHi <juniorc0105@gmail.com>
2024-03-13 09:44:16 -0400

    u

--- src/week2/app/frontend/frontend/src/app/app.routes.ts
+++ src/week2/app/frontend/frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { CounterComponent } from './pages/counter/counter.component';
+import { PrefsComponent } from './pages/prefs/prefs.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,14 @@ export const routes: Routes = [
   {
     path: 'todo-list',
     component: TodoListComponent,
+  },
+  {
+    path: 'counter',
+    component: CounterComponent,
+  },
+  {
+    path: 'prefs',
+    component: PrefsComponent,
   },
   {
     path: '**',
